76df32b9c1533acbbe3f058671cca0a50366d583
Nisarg-1629 <nisarg@gmail.com>
2026-09-18 19:01:43 +0530

    pyhton file added

---
+++ 2.py
@@ -0,0 +1,19 @@
+name = input("Enter your name ")
+age = int(input("Enter your age "))
+course = input("Enter your course ")
+percentage = float(input("Enter your percentage "))
+
+print("Name", name)
+print("Age", age)
+print("Course", course)
+print("Percentage", percentage)
+
+
+
+print("Name type", type(name))
+print("Age type", type(age))
+print("Percentage type", type(percentage))
+
+
+print("Name ID", id(name))
+print("Age ID", id(age))
